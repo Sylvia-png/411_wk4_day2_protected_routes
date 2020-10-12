01fccbd7d565eaf083f1b3edfcef07c037d57a66
sylvia <ojitoscecy.com>
2020-10-11 22:38:35 -0500

    routes

--- src/Router.js
+++ src/Router.js
@@ -8,20 +8,33 @@ import Login from './components/Login'
 
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
-
+const checkAuth = () => {
+const cookies = cookie.parse(document.cookie)
+return cookies["loggedIn"] ? true : false
+}
 
 // Write ProtectedRoute function here
+const ProtectedRoute = ({component: Component, ...rest}) => {
+return (
+<Route
+{...rest}
+render={(props) => checkAuth()
+? <Component {...props} />
+: <Redirect to="/login" />}
+/>
+)
+}
 
 
 const Router = () => {
-    return (
-        <Switch>
-            <Route path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/car/:id" component={Car} />
-        </Switch>
-    );
+return (
+<Switch>
+<Route path="/login" component={Login} />
+<Route exact path="/" component={Home} />
+<Route path="/about" component={About} />
+<Route path="/car/:id" component={Car} />
+</Switch>
+);
 };
 
 export default Router;
